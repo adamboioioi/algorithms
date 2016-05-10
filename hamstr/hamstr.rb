ae2ed1f21d1a72a573bd8be3849af23d77979f25
Ole Adam Dubnytskyy <adam.dubnitsky@gmail.com>
2016-05-10 14:24:35 +0300

    fix files reading

--- hamstr/hamstr.rb
+++ hamstr/hamstr.rb
@@ -1,5 +1,5 @@
-file = File.open('hamstr.in')
-out  = File.open('hamstr.out')
+file = ARGV[0].nil? ? "hamstr.in" : ARGV[0]
+out = ARGV[1].nil? ? "hamstr.out" : ARGV[1]
 
 input_data = file.readlines.map(&:chomp)
 food_stock = input_data.shift.to_i
